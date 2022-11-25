e76b1c3200f2224bf2e6d056f472460ff5c45b1c
JOSE SOSA <josea_1998@hotmail.com>
2022-11-24 21:59:55 -0500

    Cambios en el header

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,12 +8,12 @@ export const Nav = () => {
     <nav class="navbar-expand-lg p-1 d-flex">
         <section class="container-fluid d-flex gap-5">
           <NavbarBrand src={'../../src/assets/icon/logo-planta.png'}/>
-          <section class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav d-flex ">
-             <NavItem src={'../../src/assets/icon/icon-inicio.png'} href={'#'} contenido={'Inicio'}/>
-             <NavItem src={'../../src/assets/icon/icon-producto.png'} href={'#'} contenido={'Registrar Productos'}/>
-             <NavItem src={'../../src/assets/icon/icon-cliente.png'} href={'#'} contenido={'Registrar Clientes'}/>
-             <NavItem src={'../../src/assets/icon/icon-proovedor.png'} href={'#'} contenido={'Registrar Proovedores'}/>
+          <section class="collapse navbar-collapse d-flex justify-content-center gap-5" id="navbarNav">
+            <ul class="navbar-nav d-flex gap-5">
+             {/* <NavItem src={'../../src/assets/icon/icon-inicio.png'} href={'#'} contenido={'Inicio'}/> */}
+             <NavItem src={'../../src/assets/icon/icon-producto.png'} href={'#'} contenido={'Productos'}/>
+             <NavItem src={'../../src/assets/icon/icon-cliente.png'} href={'#'} contenido={'Clientes'}/>
+             <NavItem src={'../../src/assets/icon/icon-proovedor.png'} href={'#'} contenido={'Provedores'}/>
             </ul>
           </section>
         </section>
