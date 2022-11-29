a8fc6177832d14f5132e25340f11146381d5314b
JOSE SOSA <josea_1998@hotmail.com>
2022-11-28 20:05:44 -0500

    cambiando forma de importar img-logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { NavbarBrand } from './NavbarBrand'
 import { NavItem } from './NavItem';
 // import '../App.css'
-// import logo from '../../src/assets/icon/icon-cliente.png' 
+import logo from '../../src/assets/icon/logo-planta.png' 
 export const Nav = () => {
   return (
     <nav class="navbar navbar-expand-lg p-2 d-flex">
       <section class="container-fluid d-flex">
-        <NavbarBrand src={'../../src/assets/icon/logo-planta.png'} />
+        <NavbarBrand src={logo} />
         <section class="collapse navbar-collapse d-flex justify-content-center gap-5" id="navbarNav">
           <ul class="navbar-nav d-flex gap-5">
             {/* <NavItem src={'../../src/assets/icon/icon-inicio.png'} href={'#'} contenido={'Inicio'}/> */}
